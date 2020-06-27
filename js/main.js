852783f9cc969710672f8e2e9ebf964eef62d745
Durjoy Edward <ed310787@dpisd.org>
2020-06-27 01:06:43 -0500

    sort and load more finish

--- js/main.js
+++ js/main.js
@@ -1,10 +1,1 @@
 // load more on work page
-
-$(".workGrid .projectDiv").slice(0,3).show();
-$("#loadBtn").on('click', function () {
-    $(".workGrid  .projectDiv:hidden").slice(0,1);
-
-    if ($(".workGrid  .projectDiv:hidden").length === 0){
-        $("#loadBtn").fadeOut();
-    }
-}); 
